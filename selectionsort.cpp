746b085c6a5c227b2e21e15105b5a616f9dd3c06
Basilmp850 <basilmp850@gmail.com>
2022-10-09 09:50:30 +0530

    Added a Cpp program on selection sort using Cpp

---
+++ selectionsort.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+#define MAX 20
+
+void swap(int *a, int *b){
+
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+int main(){
+
+    int array[MAX], n;
+    cout<<"Enter the number of elements\n";
+    cin>>n;
+    cout<<"Enter the array elements\n";
+    for(int i = 0; i < n; i++){
+    
+        cin>>array[i];
+    }
+    
+    for(int i = 0; i < n-1; i++){// till second last element
+    
+        int min = i;
+        for(int j = i+1; j < n; j++){//till last element
+        
+            if(array[j] < array[min])
+                min = j;
+        }
+        
+        if(min!=i)
+            swap(&array[min], &array[i]);   
+    }
+    
+    cout<<"The sorted array is : "<<endl;
+    for(int i = 0; i < n; i++){
+    
+        cout<<array[i]<<endl;
+    }   
+    
+}
